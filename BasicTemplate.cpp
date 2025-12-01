3f1c6a68310cc4a61a9d0eb4940387d517389788
Josue Guzman <sleepyy18th@gmail.com>
2025-12-01 12:41:08 -0600

    Added basic template

---
+++ BasicTemplate.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using vi = vector<int>;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--){
+      solve();
+    }
+    return 0;
+}
